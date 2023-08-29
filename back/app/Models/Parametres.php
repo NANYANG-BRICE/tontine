f178bfef2e8ee79ea1b6a92c1ada12d312f002e8
bnanyang <nanyangbrice@gmail.com>
2023-08-29 08:43:59 +0200

    initialisation des models

---
+++ back/app/Models/Parametres.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Parametres extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'parametre';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'id',
+        'raison_sociale',
+        'nom',
+        'sigle',
+        'slogant',
+        'filigramme',
+        'logo',
+        'facebook',
+        'twitter',
+        'linkedin',
+        'site_web',
+        'whatsapp',
+        'email',
+        'telephone',
+        'entete_sms',
+        'solde_sms',
+        'solde_email',
+        'solde_whatsapp',
+        'solde_telegram',
+        'taux_annuel',
+        'taux_annuel',
+        'taux_annuel',
+        'description',
+        'status',
+        'created_at',
+        'updated_at'
+    ];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
---
+++ back/app/Models/Rembourcements.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Rembourcements extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'rembourcement';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'id',
+        'matricule',
+        'emprunt_id',
+        'telephone_emetteur',
+        'telephone_recepteur',
+        'transaction_id',
+        'operateur',
+        'montant',
+        'etat_transaction',
+        'objet',
+        'description',
+        'status',
+        'created_at',
+        'updated_at'
+    ];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
